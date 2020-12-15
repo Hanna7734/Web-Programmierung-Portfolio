1239de67391f313047a05289c2a0b81ec5d863b6
Hanna <exit>
2020-12-15 20:27:01 +0100

    New

--- app/controllers/artikels_controller.rb
+++ app/controllers/artikels_controller.rb
@@ -3,36 +3,33 @@ class ArtikelsController < ApplicationController
   before_action :authenticate_user!, except: []
   before_action :correct_user, only: [:index, :show, :edit, :update, :destroy]
 
-  # GET /artikels
-  # GET /artikels.json
+  
   def index
     @artikels = Artikel.all
   end
 
-  # GET /artikels/1
-  # GET /artikels/1.json
+ 
   def show
   end
 
-  # GET /artikels/new
+  
   def new
     @artikel = current_user.artikel.build
     #@artikel = Artikel.new
   end
 
-  # GET /artikels/1/edit
+  
   def edit
   end
 
-  # POST /artikels
-  # POST /artikels.json
+  
   def create
     #@artikel = Artikel.new(artikel_params)
     @artikel = current_user.artikel.build(artikel_params)
 
     respond_to do |format|
       if @artikel.save
-        format.html { redirect_to @artikel, notice: 'Artikel was successfully created.' }
+        format.html { redirect_to @artikel, notice: 'Artikel wurde erfolgreich erstellt.' }
         format.json { render :show, status: :created, location: @artikel }
       else
         format.html { render :new }
@@ -41,12 +38,11 @@ class ArtikelsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /artikels/1
-  # PATCH/PUT /artikels/1.json
+  
   def update
     respond_to do |format|
       if @artikel.update(artikel_params)
-        format.html { redirect_to @artikel, notice: 'Artikel was successfully updated.' }
+        format.html { redirect_to @artikel, notice: 'Artikel wurde erfolgreich aktualisiert.' }
         format.json { render :show, status: :ok, location: @artikel }
       else
         format.html { render :edit }
@@ -55,12 +51,11 @@ class ArtikelsController < ApplicationController
     end
   end
 
-  # DELETE /artikels/1
-  # DELETE /artikels/1.json
+  
   def destroy
     @artikel.destroy
     respond_to do |format|
-      format.html { redirect_to artikels_url, notice: 'Artikel was successfully destroyed.' }
+      format.html { redirect_to artikels_url, notice: 'Artikel wurde erfolgreich gelöscht.' }
       format.json { head :no_content }
     end
   end
@@ -71,12 +66,12 @@ class ArtikelsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+   
     def set_artikel
       @artikel = Artikel.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
+    
     def artikel_params
       params.require(:artikel).permit(:artikelname, :preis, :verkaeufer, :anzahl, :marke, :artikelnummer, :beschreibung, :user_id)
     end
